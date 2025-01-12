99bd400352e63c391155472b7a66be09038b82af
Black-Jade0 <160476867+Black-Jade0@users.noreply.github.com>
2025-01-12 08:55:28 +0000

    initialised

---
+++ python/data_structures.py
@@ -0,0 +1,10 @@
+class Datastructures:
+    def __init__(self):
+        self.example_list = [1,2,3,4,5]
+
+    def list_operation(self):
+        print("\n --- list operation ---")
+
+if __name =="__min__":
+    data_structures = Datastructures()
+    data_structures.list_operation()
